cefc97723006cf5133722f48c720dd147bf3b0c8
Olena Pylypenko <olena.pylypenko@gmail.com>
2023-03-30 22:24:55 -0400

    Initial framework created

---
+++ src/test/java/com/veypo/meal_planner/runners/CukesRunner.java
@@ -0,0 +1,19 @@
+package com.veypo.meal_planner.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"html:target/cucumber-report.html", //html report
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "json:target/cucumber.json",
+                "rerun:target/rerun.txt"}, // cucumber report
+        features = "src/test/resources/features",
+        glue = "com/veypo/meal_planner/step_definitions",
+        dryRun = false,
+        tags = "@wip"
+)
+public class CukesRunner {
+}
---
+++ src/test/java/com/veypo/meal_planner/runners/FailedTestRunner.java
@@ -0,0 +1,13 @@
+package com.veypo.meal_planner.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "@target/rerun.txt",
+        glue = "src/test/resources/features"
+)
+public class FailedTestRunner {
+}
